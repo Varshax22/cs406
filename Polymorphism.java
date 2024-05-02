c62f9b9fb601e232d29b92965918503bac65a19c
Varshax22 <168709718+Varshax22@users.noreply.github.com>
2024-05-02 23:21:15 +0530

    8 th

---
+++ Polymorphism.java
@@ -0,0 +1,22 @@
+class Birds {
+    public void fly() {
+        System.out.println("All Birds can fly except 'Ostrich' and 'Kiwi'");
+    }
+
+    public void fly(String s) {
+        System.out.println("Sparrow,Parrot,Crow all can fly");
+    }
+
+    public void fly(String s,String d){
+        System.out.println("Ostrich and Kiwi can't fly");
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Birds obj = new Birds();
+        obj.fly("a","b");
+        obj.fly("a");
+        obj.fly();
+    }
+}
